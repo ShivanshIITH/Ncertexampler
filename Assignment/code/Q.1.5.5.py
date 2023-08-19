9c4653f8cad385220568653590bb11205e40c7a3
ShivanshIITH <142160542+ShivanshIITH@users.noreply.github.com>
2023-08-20 00:28:22 +0530

    Delete Q.1.5.5.py

--- Assignment/code/Q.1.5.5.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-B = np.array([[-4],[6]])  
-I=(1/(np.sqrt(37) + 4 + np.sqrt(61)))*np.array([[np.sqrt(61) - 16 - 3*np.sqrt(37)],[-np.sqrt(61) + 24 - 5*np.sqrt(37)]])    #Here I is incentre (point of intersection of angle bisectors)
-n = np.array([[11], [1]])   #n is normal vector 
-nT = n.T   #taking transpose of n
-r = float(abs((nT @ I) - (nT @ B))/(np.linalg.norm(n)))  #r is distance between I and BC (nT.I - nT.B=0, n and I are vectors)
-print("Distance of I from BC = "+str(r))
